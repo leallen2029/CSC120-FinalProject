8ed472203bd037ea6b1a4cf607332a04605f3175
Landree Allen <leallen@smith.edu>
2026-04-12 14:36:11 -0400

    save

--- Intro.java
+++ Intro.java
@@ -1,6 +1,20 @@
 import java.util.Scanner;
 
-public class Intro {
+public class Intro implements narrative {
+    public static void main(String[] args) {
+        System.out.println(narratives.get(0));
+        System.out.println(narratives.get(1));
+        Scanner input = new Scanner(System.in);
+        String transportChoice = input.nextLine();
+        if (transportChoice.equalsIgnoreCase("cab")) {
+            System.out.println("You hail a cab and head to the crime scene.");
+        } else if (transportChoice.equalsIgnoreCase("walk")) {
+            System.out.println("You decide to walk to the crime scene, taking in the sights of London along the way.");
+        } else {
+            System.out.println("Invalid choice. Please choose 'cab' or 'walk'.");
+        }
+        input.close();
+    }
 
     
 }
--- narrative.java
+++ narrative.java
@@ -2,7 +2,15 @@ import java.util.ArrayList;
 
 public class narrative {
     ArrayList<String> narratives = new ArrayList<String>();
-    String wakingUp = "You wake up in a dark room. You have no memory of how you got there.";
+
+    String wakingUp = """You wake up up in the dark of you B Baker Street flat. 
+    As you pick up the phone as you hear Detective Inspector Lestrade tell you there has been another death that he wants you to come look at. 
+    You get out of bed and get ready to go to the crime scene. 
+    You grab your coat, hat, and John Watson, heading out the door."""
+
     narratives.add(wakingUp);
+    
+    String transportOption = "How would you like to get to the crime scene? You can take a cab or walk";
+    narratives.add(transportOption);
 
 }

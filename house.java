c8b7e34b583cd5002f32316f4b0035bdc44b236a
Landree Allen <leallen@smith.edu>
2026-04-16 13:17:38 -0400

    house_outline

--- house.java
+++ house.java
@@ -1,3 +1,49 @@
-public class house {
-    
+public class House extends Scene {
+
+    public House() {
+        super("House", "You arrive at the scene of the death.");
+    }
+
+    @Override
+    public void lookAround() {
+        super.lookAround();
+        System.out.println("You see the outside of the house, the inside, a staircase, a bedroom, something on the floor, and the body.");
+    }
+
+    @Override
+    public void inspect(String target) {
+
+        if (!hasLookedAround()) {
+            System.out.println("You should look around first.");
+            return;
+        }
+
+        if (target.equalsIgnoreCase("body")) {
+            System.out.println("The body lies still. Something about it feels off.");
+        }
+
+        else if (target.equalsIgnoreCase("floor")) {
+            System.out.println("There are marks on the floor. They look intentional.");
+        }
+
+        else if (target.equalsIgnoreCase("bedroom")) {
+            System.out.println("The bedroom looks disturbed, but not randomly.");
+        }
+
+        else if (target.equalsIgnoreCase("stairs")) {
+            System.out.println("The stairs creak. Someone was here recently.");
+        }
+
+        else if (target.equalsIgnoreCase("outside")) {
+            System.out.println("Outside, everything seems normal. Too normal.");
+        }
+
+        else if (target.equalsIgnoreCase("inside")) {
+            System.out.println("Inside, the air feels heavy. Something happened here.");
+        }
+
+        else {
+            System.out.println("Nothing important there.");
+        }
+    }
 }

a71b186bc470898b4c2f171025fa7a630a4a55f7
Landree Allen <leallen@smith.edu>
2026-04-16 13:28:21 -0400

    main_test

--- main.java
+++ main.java
@@ -1,3 +1,41 @@
+import java.util.Scanner;
+
 public class Main {
-    
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        // Create your scene
+        House house = new House();
+
+        // Show intro
+        house.displayScene();
+
+        while (!house.isCompleted()) {
+
+            System.out.print("\nWhat do you want to do? ");
+            String command = input.nextLine();
+
+            if (command.equalsIgnoreCase("look")) {
+                house.lookAround();
+            }
+
+            else if (command.startsWith("inspect ")) {
+                String target = command.substring(8);
+                house.inspect(target);
+            }
+
+            else if (command.equalsIgnoreCase("continue")) {
+                house.completeScene();
+            }
+
+            else {
+                System.out.println("Unknown command.");
+            }
+        }
+
+        System.out.println("You leave the house...");
+
+        input.close();
+    }
 }

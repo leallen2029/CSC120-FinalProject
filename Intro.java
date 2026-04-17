5cd11250ea84917b9682221d87bb3edfdb183797
Landree Allen <leallen@smith.edu>
2026-04-17 19:56:55 -0400

    what

--- Intro.java
+++ Intro.java
@@ -4,7 +4,8 @@ public class Intro {
 
     String wakingUp = """
     You wake up up in the dark of you 221B Baker Street flat. 
-    As you pick up the phone as you hear Detective Inspector Lestrade tell you there has been another death that he wants you to come look at. 
+    As you pick up the phone as you hear Detective Inspector Lestrade 
+    tell you there has been another death that he wants you to come look at. 
     You get out of bed and get ready to go to the crime scene. 
     You grab your coat, hat, and John Watson, heading out the door.
     """;

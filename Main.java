590af3628e044680681000d3862862ae0b0601d3
Landree Allen <leallen@smith.edu>
2026-04-19 13:20:44 -0400

    fix

--- Main.java
+++ Main.java
@@ -4,13 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        Scene currentScene = new Intro();
+        Intro currentScene = new Intro();
 
-        currentScene.displayScene();
+        currentScene.showIntroText(); 
 
         while (!currentScene.isCompleted()) {
             System.out.print("\nWhat do you want to do? ");
             String command = input.nextLine();
+
             currentScene.handleCommand(command);
         }
 

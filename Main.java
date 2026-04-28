d7a4f9fb08f8aacfdd24d9da2ae3a6564efb4c71
Landree Allen <leallen@smith.edu>
2026-04-28 12:55:18 -0400

    save

--- Main.java
+++ Main.java
@@ -2,9 +2,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Key commands:");
         System.out.println("\n(Type 'help' at any time to see available commands.)");
-        System.out.println("Tip: Type 'write <your note>' to add something to your journal.");
         System.out.println("Type 'journal' to read your journal.");
+        System.out.println("Type 'write <your note>' to add something to your journal.");
+        
         Scanner input = new Scanner(System.in);
         Player player = new Player();
 
